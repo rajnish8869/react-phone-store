cefb4f74fe6a6cfd48a0ef2326e631f5d9a63569
rajnish8869 <35104535+rajnish8869@users.noreply.github.com>
2020-04-17 17:04:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,14 @@ export default class Navbar extends Component {
     render(){
         return (
             <NavWrapper className="navbar navbar-expand-sm navbar-dark p-sm-10">
-                {/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */}
+                
             <Link to="/">
                 <img src={logo} alt="store" className="navbar-brand" />
             </Link>
             <ul className="navbar-nav align-items-center">
                 <li className="nav-item ml-5">
                 <Link to="/"className="nav-link">
-                    Products
+                    <div className="productBtn px-3 py-1">Products</div>
                 </Link>
                 </li>
             </ul>
@@ -41,5 +38,10 @@ background-color: var(--mainBlue);
     color: var(--mainWhite) !important;
     font-size: 1.3rem;
     text-transform: capitalize;
+    
+}
+.productBtn {
+    border: 2px solid white;
+    border-radius: 16px;
 }
 `
